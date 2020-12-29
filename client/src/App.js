e6d5dbf67d3b32c6395a14140944b4207c7c40b3
Tamzid <tamzidhossainxarir@gmail.com>
2020-12-29 12:23:35 +0600

    Frist Commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CreateRoom from "./routes/CreateRoom";
+import Room from "./routes/Room";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={CreateRoom} />
+          <Route path="/room/:roomID" component={Room} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
